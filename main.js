7d7946581adc034f1d64b90333f4e3e84332a21a
Michelle Wang <michellewang0402@gmail.com>
2023-02-15 15:46:41 -0500

    finished

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // set frame height, frame width, and margins
-const FRAME_HEIGHT = 200;
-const FRAME_WIDTH = 500;
-const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
+const FRAME_WIDTH = 200;
+const FRAME_HEIGHT = 500;
+const MARGINS = {left: 100, right: 50, top: 50, bottom: 50};
 
 // list of data points
 const data = [55000, 48000, 27000, 66000, 90000];
@@ -19,27 +19,29 @@ const FRAME3 = d3.select("#vis1")
 
 // scaling functions 
 // max looks through all the dataset points
-const MAX_X = d3.max(data, (d) => {return d;});
-console.log("Max x: " + MAX_X);
+const MAX_Y = d3.max(data, (d) => {return d;});
+console.log("Max x: " + MAX_Y);
 
 // scale function
 // scale linear used for quantitative data
 // domain is input, range is output
-const X_SCALE = d3.scaleLinear()
-					.domain([0, (MAX_X + 10000)])
-					.range([0, VIS_WIDTH]);
+const Y_SCALE = d3.scaleLinear()
+					.domain([0, (MAX_Y + 10000)])
+					.range([0, VIS_HEIGHT]);
 
 //plot
 FRAME3.selectAll("points")
 		.data(data)
 		.enter()
 		.append("circle")
-			.attr("cx", (d) => {
-				return (X_SCALE(d) + MARGINS.left);
+			.attr("cx", MARGINS.left + 50)
+			.attr("cy", (d) => {
+				return(Y_SCALE(d) + MARGINS.top)
 			})
-			.attr("cy", MARGINS.top)
 			.attr("r", 10)
 			.attr("class", "point")
+
+
 
 // add an axis
 // g is used for a placeholder
@@ -47,8 +49,8 @@ FRAME3.append("g")
 		// attribute transform - moves horizontal and vertically
 		.attr("transform", 
 			// how far to translate horizontal (move over past the left margin) and vertical move past viz heights plus the margin
-			"translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
+			"translate(" + MARGINS.left + "," + (MARGINS.bottom) + ")")
 		// call the axisBottom function and make an axis for the x_scale function and make 4 ticks
-		.call(d3.axisBottom(X_SCALE).ticks(4))
+		.call(d3.axisLeft(Y_SCALE).ticks(4))
 			// technically not in-line but you can also give it a class and style it in css
-			.attr("font-size", "20px");
+			.attr("font-size", "10px");
